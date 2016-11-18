04b8d0313f13b84952d471de57bb20a2713b24a2
MonkeuJava <vasy_nn@mail.ru>
2016-11-18 21:45:32 +0700

    user input

--- Calculate.java
+++ Calculate.java
@@ -4,6 +4,10 @@ public class Calculate{
 		int first = Integer.valueOf(arg[0]);
 		int second = Integer.valueOf(arg[1]);
 		int summ = first+second;
+		double i = Double.valueOf(arg[2]);
+		double j = summ / i;
+		
 		System.out.println("Sum " + summ);
+		System.out.println("Result " + j);
 		}
 }
