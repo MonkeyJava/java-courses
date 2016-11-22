b04d27fb3c09bd768df5332fff29ad4d1ca4b5d2
MonkeuJava <vasy_nn@mail.ru>
2016-11-22 20:53:04 +0700

    user input2

--- Calculator.java
+++ Calculator.java
@@ -10,10 +10,31 @@ public class Calculator {
 	Суммируем аргументы.
 	@param params Аргументы суммирования.
 	*/
-	public void add(int ... params){
-		for(Integer param:params){
-			this.result +=param;
-		}
+	public void add(int first, int second){
+		
+			this.result = first + second;
+	}
+	/**Вычитаем аргументы.
+	@param params Аргументы вычитания.
+	*/
+	public void subtract(int first, int second){
+			
+			this.result = first - second;
+	}
+	/**Умножаем аргументы.
+	@param params Аргументы умножения.
+	*/
+	public void multiplication (int first, int second){
+		
+			this.result = first * second;
+	}
+	/**Делим аргументы.
+	@param params Аргументы деления.
+	*/
+	public void division(int first, int second){
+
+			this.result = first / second;
+		
 	}
 	/**
 	Получить результат.
--- InteractRunner.java
+++ InteractRunner.java
@@ -11,12 +11,29 @@ public class InteractRunner{
 		try{
 			Calculator calc = new Calculator();
 			String exit = "no";
+			
 			while(!exit.equals("yes")){
 				System.out.println("Enter first arg:");
 				String first = reader.next();
 				System.out.println("Enter second arg:");
 				String second = reader.next();
-				calc.add(Integer.valueOf(first), Integer.valueOf(second));
+				System.out.println("What operation to execute addition, subtraction, multiplication, division?: ");
+				String choise = reader.next();
+				
+				if (choise.equals("addition")){
+					calc.add(Integer.valueOf(first), Integer.valueOf(second));
+				}
+				else if (choise.equals("subtract")){
+						calc.subtract(Integer.valueOf(first), Integer.valueOf(second));
+					}
+					else if (choise.equals("multiplication")){
+							calc.multiplication(Integer.valueOf(first), Integer.valueOf(second));
+							}
+							else  if (choise.equals("division")){
+								calc.division(Integer.valueOf(first), Integer.valueOf(second));
+							}
+								else System.out.println("Erorr!!");
+				
 				System.out.println("Result: " + calc.getResult());
 				calc.cleanResult();
 				System.out.println("Exit: yes/no");
